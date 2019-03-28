309012d17c30c1bae7ef0d1007ff084a758fc18e
Richard Hpa <richard.hpa@hotmail.com>
2019-03-28 14:24:03 +1300

    added stopwatch project done in class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+var timerInt;
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            timerOn: false,
+            timerText: 0,
+            buttonText: 'Start Stop Watch',
+            buttonClass: 'btn btn-primary'
+        }
+        this.toggleTimer = this.toggleTimer.bind(this);
+    }
+
+    toggleTimer(){
+        if(this.state.timerOn === false){
+
+            timerInt = setInterval(
+                () => this.tick(),1000
+            );
+            this.setState({
+                timerOn: true,
+                buttonText: 'Stop Timer',
+                buttonClass: 'btn btn-warning'
+            })
+
+        } else {
+
+            clearInterval(timerInt);
+            this.setState({
+                timerOn: false,
+                buttonText: 'Continue Timer',
+                buttonClass: 'btn btn-primary'
+            })
+        }
+    }
+
+    tick(){
+        var newTime = this.state.timerText + 1;
+        this.setState({
+            timerText: newTime
+        })
+    }
+
+
+
   render() {
+      let { buttonText, buttonClass } = this.state;
+      let button;
+      if( (this.state.timerOn === false) && (this.state.timerText > 0) ){
+          button = <button className="btn btn-danger">Reset Stop Watch</button>;
+      }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="d-flex align-items-center h-100 bg-dark">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-8">
+                    <div className="card text-center">
+                        <h1 className="display-4">Stop Watch</h1>
+                        <h2 className="display-2">{this.state.timerText}</h2>
+                        <button className={buttonClass} onClick={this.toggleTimer} >{buttonText}</button>
+                        {button}
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
